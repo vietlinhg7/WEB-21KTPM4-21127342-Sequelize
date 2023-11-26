5f196383f9b5e9057f69d749869db358e84a1fec
Nguyen Duc Viet Linh <44722910+vietlinhg7@users.noreply.github.com>
2023-11-26 16:42:10 +0700

    Add files via upload

---
+++ routes/blogRouter.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const controller = require("../controllers/blogController");
+
+router.get("/", controller.showList);
+router.get("/:id", controller.showDetails);
+module.exports = router;
